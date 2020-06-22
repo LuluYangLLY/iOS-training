0298247c9bf25fa9df0841b383a3ccf6e018f202
lulu.yang <fengshiyunji@foxmail.com>
2020-06-22 21:54:05 +0800

    Step1 Init a new project

---
+++ moments/moments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  moments
+//
+//  Created by Yang Lulu  on 2020/6/22.
+//  Copyright © 2020 Yang Lulu . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
